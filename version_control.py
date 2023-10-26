2f4c78eaeecf5195c5d728963fb28b22520d603c
danie1park <danielpark146@gmail.com>
2023-10-26 17:10:25 -0400

    To Daniel's repository

--- version_control.py
+++ version_control.py
@@ -30,15 +30,6 @@ def encode(password):
 
 
 def decode(password):
-    # decoded_password = ''
-    # for char in password:
-    #     if char > '2':
-    #         new_char = str(int(char) - 3)
-    #         decoded_password += new_char
-    #     else:
-    #         new_char = str(int(char) + 7)
-    #         decoded_password += new_char
-    # return decoded_password
     return None
 
 
